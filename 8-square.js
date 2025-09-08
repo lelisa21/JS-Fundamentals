86b688c2da568a112c0391ebebcf88d29cd41b3a
Lelisa Hailu <189599041+lelisa21@users.noreply.github.com>
2025-09-08 17:31:21 +0300

    comment removed  8-square.js

--- 8-square.js
+++ 8-square.js
@@ -11,16 +11,3 @@ if(isNaN(square)){
     console.log(row);
   }
 }
-
-
-// or by using builtin Repeat function 
-
-// const size = parseInt(process.argv[2]);
-
-// if (isNaN(size)) {
-//     console.log("Missing size");
-// } else {
-//     for (let i = 0; i < size; i++) {
-//         console.log('X'.repeat(size));
-//     }
-// }
