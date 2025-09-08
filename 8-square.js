44661625811a03ebd859bc5e912f2eb108f6f4e5
Lelisa Hailu <189599041+lelisa21@users.noreply.github.com>
2025-09-08 17:34:19 +0300

    used While loop 8-square.js

--- 8-square.js
+++ 8-square.js
@@ -1,13 +1,17 @@
 const square = parseInt(process.argv[2]);
 
-if(isNaN(square)){
-  console.log("Missing size");
-}else{
-  for(let i = 0 ; i < square; i++){
-    let row = "";
-    for(let j = 0; j < square; j++){
-      row += "X";
+if (isNaN(square)) {
+    console.log("Missing size");
+} else {
+    let i = 0;
+    while (i < square) {
+        let row = "";
+        let j = 0;
+        while (j < square) {
+            row += "X";
+            j++;
+        }
+        console.log(row);
+        i++;
     }
-    console.log(row);
-  }
 }
